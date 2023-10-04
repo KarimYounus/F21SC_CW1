3387aa3c9c8fd1fa925c9a20d2b00465e3d98fe1
Karim Younus <younus.karim35@gmail.com>
2023-10-04 18:22:43 +0100

    Added basic HTTP Request with a button that makes the request and displays the HTML content in a message box.

---
+++ CW1-F21SC/HttpFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CW1_F21SC;
+
+public class HttpFunctions
+{
+    private readonly HttpClient _httpClient = new();
+    
+    //Send a GET request to the specified URL and return the response as a string
+    public async Task<string> SendGetRequest(string url)
+    {
+        var response = await _httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
---
+++ CW1-F21SC/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CW1_F21SC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private readonly HttpFunctions _httpFunctions;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            _httpFunctions = new HttpFunctions();
+        }
+        
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://www.google.com";
+            var response = await _httpFunctions.SendGetRequest(url);
+            MessageBox.Show(response);
+        }
+        
+    }
+}
